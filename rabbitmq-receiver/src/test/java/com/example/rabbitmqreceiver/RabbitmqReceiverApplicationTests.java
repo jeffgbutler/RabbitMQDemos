6a5bcb6aee1bf2a41bf9fd4340cdf832338e5e2c
Jeff Butler <jeffgbutler@gmail.com>
2022-08-22 17:26:42 -0400

    Add receiver application

---
+++ rabbitmq-receiver/src/test/java/com/example/rabbitmqreceiver/RabbitmqReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
